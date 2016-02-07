5c01caf93272d1826aa013fb0985fc999a106d73
Tim Bachofen <tim.bachofen@gmail.com>
2016-02-07 14:30:26 +0100

    python solution 4

---
+++ 4/palindrome_product.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+result = 0
+i = j = 100
+
+def is_palindrome(s):
+   left, right = s[:len(s)//2], s[len(s)//2:]
+   if left == right[::-1]:
+      return True
+   return False
+
+while i < 1000:
+   while j < 1000:
+      k = i * j
+      if is_palindrome(str(k)) and k > result:
+         result = k
+      j += 1
+   j = 100
+   i += 1
+
+print(result)
